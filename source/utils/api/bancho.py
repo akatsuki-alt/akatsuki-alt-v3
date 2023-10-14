ea7990c700f9fa2cb3dc35908397c7e91dd31ae8
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-10-14 18:25:23 +0000

    disable ossapi logging

--- source/utils/api/bancho.py
+++ source/utils/api/bancho.py
@@ -1,4 +1,6 @@
 from ossapi import Ossapi
+import logging
 import config
 
 client = Ossapi(client_id=config.OSU_CLIENT_ID, client_secret=config.OSU_API_KEY, token_directory=config.BASE_PATH+"/")
+client.log.level = logging.ERROR
