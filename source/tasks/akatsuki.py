c237e0cc0aaf8c31a7899540e9fb0541b34181f9
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-10-21 13:23:33 +0000

    remove linter hint

--- source/tasks/akatsuki.py
+++ source/tasks/akatsuki.py
@@ -377,7 +377,6 @@ def update_user(session, user_id: int, mode: int, relax: int, date: date, user_i
             session.merge(score_to_db(score, user_id, mode, relax), load=True)
         scores = {}
         for score in session.query(DBScore).filter(DBScore.server == "akatsuki", DBScore.completed == 3, DBScore.user_id == user_id).all():
-            score: DBScore = score
             mode_str = f'{score.mode}+{score.relax}'
             if score.beatmap_id in scores:
                 if mode_str in scores[score.beatmap_id]:
