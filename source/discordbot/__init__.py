67939a2d9c77dd985969f74a19e0e4a301663155
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-10-28 22:44:09 +0000

    add !showserverclears command

--- source/discordbot/__init__.py
+++ source/discordbot/__init__.py
@@ -1,14 +1,14 @@
+from discordbot.commands.showclears import ShowClearsCommand, ShowServerClearsCommand
+from discordbot.commands.leaderboard import ShowLeaderboardCommand
 from discordbot.commands.show import ShowCommand, ResetCommand
 from discordbot.commands.setdefault import SetDefaultCommand
+from discordbot.commands.addqueue import AddQueueCommand
 from discordbot.commands.servers import ServersCommand
 from discordbot.commands.recent import RecentCommand
+from discordbot.commands.show1s import Show1sCommand
 from discordbot.commands.ping import PingCommand
 from discordbot.commands.link import LinkCommand
 from discordbot.commands.help import HelpCommand
-from discordbot.commands.leaderboard import ShowLeaderboardCommand
-from discordbot.commands.show1s import Show1sCommand
-from discordbot.commands.showclears import ShowClearsCommand
-from discordbot.commands.addqueue import AddQueueCommand
 import discordbot.bot as bot
 
 bot.commands.append(PingCommand())
@@ -22,4 +22,5 @@ bot.commands.append(RecentCommand())
 bot.commands.append(ShowLeaderboardCommand())
 bot.commands.append(Show1sCommand())
 bot.commands.append(ShowClearsCommand())
+bot.commands.append(ShowServerClearsCommand())
 bot.commands.append(AddQueueCommand())
--- source/discordbot/commands/showclears.py
+++ source/discordbot/commands/showclears.py
@@ -12,12 +12,13 @@ from utils.mods import get_mods_simple
 
 class ClearsView(discord.ui.View):
     
-    def __init__(self, api_options):
+    def __init__(self, api_options, all=False):
         super().__init__()
         self.types = [e.value for e in ScoreSortEnum]
         self.type = 4
         self.desc = True
         self.api_options = api_options
+        self.all = all
         self.page = 1
     
     @discord.ui.button(label="Previous",style=discord.ButtonStyle.gray)
@@ -54,7 +55,7 @@ class ClearsView(discord.ui.View):
         self.page = 1
         await interaction.message.edit(embed=self.get_embed(), view=self)
 
-    def get_embed(self):
+    def get_embed_user(self):
         embed = Embed(title="Clears")
         scores = instance.get_user_clears(
             user_id=self.api_options['user_id'],
@@ -87,6 +88,48 @@ class ClearsView(discord.ui.View):
         content += '```'
         embed.description = content
         return embed
+    
+    def get_embed_all(self):
+        embed = Embed(title="Clears")
+        scores = instance.get_all_clears(
+            server=self.api_options['server'],
+            mode=self.api_options['mode'],
+            relax=self.api_options['relax'],
+            sort=self.types[self.type],
+            page=self.page,
+            length=10,
+            desc=self.desc,
+            score_filter=self.api_options['score_filter'],
+            beatmap_filter=self.api_options['beatmap_filter'],
+        )
+        content = '```'
+        if not scores or not scores[1]:
+            content += 'No scores :(\n```'
+            embed.description = content
+            return embed
+        embed.title += f" ({scores[0]})"
+        with postgres.instance.managed_session() as session:
+            for score in scores[1]:
+                if (beatmap := load_beatmap(session, score.beatmap_id)) is not None:
+                    username = "Unknown Player:"
+                    if (user := instance.get_user_info(server=self.api_options['server'], user_id=score.user_id)):
+                        username = user.username
+                    title = f"{username}: {beatmap.title} [{beatmap.version}]"
+                    max_combo = beatmap.max_combo
+                else:
+                    title = "API error :("
+                    max_combo = 42069
+                content += f"{title} +{''.join(get_mods_simple(score.mods))}\n"
+                content += f"{score.rank} {score.combo}/{max_combo}x [{score.count_300}/{score.count_100}/{score.count_50}/{score.count_miss}] {score.accuracy:.2f}% {score.score:,} {score.pp}pp\n"
+        content += '```'
+        embed.description = content
+        return embed
+    
+    def get_embed(self):
+        if self.all:
+            return self.get_embed_all()
+        else:
+            return self.get_embed_user()
     
     async def reply(self, message: Message):
         await message.reply(embed=self.get_embed(), view=self)
@@ -129,3 +172,35 @@ class ShowClearsCommand(Command):
         if 'beatmap_filter' in parsed:
             beatmap_filter = parsed['beatmap_filter']
         await ClearsView({'user_id': user_id, 'server': server, 'mode': mode, 'relax': relax, 'score_filter': score_filter, 'beatmap_filter': beatmap_filter}).reply(message)
+
+class ShowServerClearsCommand(Command):
+    
+    def __init__(self) -> None:
+        super().__init__("showserverclears", "shows every clears", ['showserverclears', 'serverclears'])
+    
+    async def run(self, message: Message, arguments: List[str]):
+        parsed = parse_args(arguments, unparsed=True)
+        if (link := self.get_link(message)) is None:
+            await message.reply(f"You don't have an account linked!")
+            return
+        modes = {'std': (0,0), 'std_rx': (0,1), 'std_ap': (0,2), 'taiko': (1,0), 'taiko_rx': (1,1), 'ctb': (2,0), 'ctb_rx': (2,1), 'mania': (3,0)}
+        mode = link.default_mode
+        relax = link.default_relax
+        server = link.default_server
+        score_filter = ''
+        beatmap_filter = ''
+        if parsed['unparsed']:
+            if parsed['unparsed'][0] not in modes:
+                await message.reply(f"Invalid mode! Valid modes: {','.join(modes.keys())}")
+                return
+            mode, relax = modes[parsed['unparsed'][0]]
+        if 'server' in parsed:
+            if (server := self.get_server(parsed['server'])) is None:
+                await message.reply("Unknown server!")
+                return
+            server = server.server_name
+        if 'score_filter' in parsed:
+            score_filter = parsed['score_filter']
+        if 'beatmap_filter' in parsed:
+            beatmap_filter = parsed['beatmap_filter']
+        await ClearsView(all=True, api_options={'server': server, 'mode': mode, 'relax': relax, 'score_filter': score_filter, 'beatmap_filter': beatmap_filter}).reply(message)
