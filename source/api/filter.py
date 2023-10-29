fd4f5b31c91e6208a221a6cab92967856f41d18c
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-10-29 22:02:45 +0000

    fix ranked status check on filter

--- source/api/filter.py
+++ source/api/filter.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm.query import Query
+from sqlalchemy import Integer
 from typing import TypedDict
 import re
 
@@ -10,15 +11,29 @@ def build_query(query: Query, model, conditions=[]):
             if op in condition:
                 break
         attr, to_match = condition.split(op)
-        match op:
-            case "!=":
-                query = query.filter(getattr(model, attr) != to_match)
-            case "==":
-                query = query.filter(getattr(model, attr) == to_match)
-            case "<":
-                query = query.filter(getattr(model, attr) < to_match)
-            case ">":
-                query = query.filter(getattr(model, attr) > to_match)
-            case "in":
-                query = query.filter(to_match in getattr(model, attr))
-    return query
+        if "." in attr:
+            attr, attr2 = attr.split(".")
+            match op:
+                case "!=":
+                    query = query.filter(getattr(model, attr)[attr2].astext.cast(Integer) != to_match)
+                case "==":
+                    query = query.filter(getattr(model, attr)[attr2].astext.cast(Integer) == to_match)
+                case "<":
+                    query = query.filter(getattr(model, attr)[attr2].astext.cast(Integer) < to_match)
+                case ">":
+                    query = query.filter(getattr(model, attr)[attr2].astext.cast(Integer) > to_match)
+                case "in":
+                    query = query.filter(to_match in getattr(model, attr))
+        else:
+            match op:
+                case "!=":
+                    query = query.filter(getattr(model, attr) != to_match)
+                case "==":
+                    query = query.filter(getattr(model, attr) == to_match)
+                case "<":
+                    query = query.filter(getattr(model, attr) < to_match)
+                case ">":
+                    query = query.filter(getattr(model, attr) > to_match)
+                case "in":
+                    query = query.filter(to_match in getattr(model, attr))
+        return query
