a96d76aae21dfbe23240d54cb8ea64ad09d7dc89
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-10-30 03:01:11 +0000

    add has/has not and custom mods handler for api filters

--- source/api/filter.py
+++ source/api/filter.py
@@ -1,9 +1,8 @@
 from sqlalchemy.orm.query import Query
 from sqlalchemy import Integer
-from typing import TypedDict
-import re
+import utils.mods as mods
 
-OPS = ['!=', '==', '<', '>', 'in'] 
+OPS = ['!=', '==', '<', '>', 'has not', 'has']
 
 def build_query(query: Query, model, conditions=[]):
     for condition in conditions:
@@ -11,6 +10,8 @@ def build_query(query: Query, model, conditions=[]):
             if op in condition:
                 break
         attr, to_match = condition.split(op)
+        attr = attr.strip()
+        to_match = to_match.strip()
         if "." in attr:
             attr, attr2 = attr.split(".")
             match op:
@@ -22,8 +23,22 @@ def build_query(query: Query, model, conditions=[]):
                     query = query.filter(getattr(model, attr)[attr2].astext.cast(Integer) < to_match)
                 case ">":
                     query = query.filter(getattr(model, attr)[attr2].astext.cast(Integer) > to_match)
-                case "in":
+                case "has":
                     query = query.filter(to_match in getattr(model, attr))
+                case "has not":
+                    query = query.filter(to_match not in getattr(model, attr))
+        elif attr == "mods":
+            mods_bit = mods.mods_from_string(to_match)
+            print(mods_bit)
+            match op:
+                case "!=":
+                    query = query.filter(getattr(model, attr) != mods_bit)
+                case "==":
+                    query = query.filter(getattr(model, attr) == mods_bit)
+                case "has":
+                    query = query.filter(getattr(model, attr).op("&")(mods_bit) > 0)
+                case "has not":
+                    query = query.filter(getattr(model, attr).op("&")(mods_bit) == 0)
         else:
             match op:
                 case "!=":
@@ -34,6 +49,8 @@ def build_query(query: Query, model, conditions=[]):
                     query = query.filter(getattr(model, attr) < to_match)
                 case ">":
                     query = query.filter(getattr(model, attr) > to_match)
-                case "in":
+                case "has":
                     query = query.filter(to_match in getattr(model, attr))
+                case "has not":
+                    query = query.filter(to_match not in getattr(model, attr))
         return query
