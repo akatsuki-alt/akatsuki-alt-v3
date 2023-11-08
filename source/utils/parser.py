2a22bb6d03fe2f2a3d1e5034d526dd4b467ab8f9
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-11-08 05:28:59 +0000

    add parse akatsuki embed util

--- source/utils/parser.py
+++ source/utils/parser.py
@@ -1,4 +1,5 @@
 from typing import *
+from utils.selfbot import Embed
 
 def parse_args(args: List[str], unparsed=False):
     parsed_args = {}
@@ -12,3 +13,16 @@ def parse_args(args: List[str], unparsed=False):
             continue
         parsed_args[arg[0]] = arg[1]
     return parsed_args
+
+def parse_akatsuki_embed(embed: Embed) -> [int, str]:
+    if 'url' in embed and embed['url']: # new embed format
+        beatmap_id = int(embed['url'].split("/")[-1])
+        nominator = embed['author']['name'].split()[0]
+        return beatmap_id, nominator
+    else:
+        if embed['fields'][3]['name'] == "Gamemode": # very old embed format
+            beatmap_id = int(embed['fields'][4]['value'].split("/")[-1][:-1])
+        else:
+            beatmap_id = int(embed['fields'][3]['value'].split("/")[-1][:-1])
+        nominator = embed['fields'][2]['value'].split("]")[0][1:]
+        return beatmap_id, nominator
