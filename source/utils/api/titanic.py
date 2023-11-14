d3d25c5993202cb4043dce73d8c0c2eac8e569e1
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-11-14 00:09:23 +0000

    add get_user_recent method in titanic api

--- source/utils/api/titanic.py
+++ source/utils/api/titanic.py
@@ -55,6 +55,72 @@ class Profile(TypedDict):
     silence_end: str
     stats: List[GameStats]
 
+class Beatmapset(TypedDict):
+    
+    approved_at: str
+    artist: str
+    available: bool
+    created_at: str
+    creator: str
+    genre_id: int
+    has_storyboard: bool
+    has_video: bool
+    id: int
+    language_id: int
+    last_update: str
+    osz_filesize: int
+    osz_filesize_novideo: int
+    server: int # 0: uploaded on bancho
+    source: str
+    status: int
+    tags: str
+    title: str
+
+class Beatmap(TypedDict):
+    
+    beatmapset: Beatmapset
+    ar: float
+    bpm: float
+    created_at: str
+    cs: float
+    diff: float
+    filename: str
+    hp: float
+    id: int
+    last_update: str
+    max_combo: int
+    md5: str
+    mode: int
+    od: float
+    passcount: int
+    playcount: int
+    set_id: int
+    status: int
+    total_length: int
+    version: str
+    
+class Score(TypedDict):
+
+    beatmap: Beatmap
+    acc: float
+    grade: str
+    id: int
+    max_combo: int
+    mode: int
+    mods: int
+    n300: int
+    n100: int
+    n50: int
+    nGeki: int
+    nKatu: int
+    nMiss: int
+    perfect: bool
+    pinned: bool
+    pp: float
+    submitted_at: str
+    total_score: int
+    user_id: int    
+
 def lookup_user(username: str) -> int | None:
     req = handler.get(f"https://osu.lekuru.xyz/u/{username}")
     if not req.ok:
@@ -67,3 +133,9 @@ def get_user_info(user_id: int) -> Profile | None:
         return
     return req.json()
 
+def get_user_recent(user_id: int, mode: int) -> List[Score] | None:
+    modes = ['osu', 'taiko', 'fruits', 'mania']
+    req = handler.get(f"https://osu.lekuru.xyz/api/profile/{user_id}/recent/{modes[mode]}")
+    if not req.ok:
+        return
+    return req.json()
